67ffe56c9ab09e63eebd726bf712bbc22ba343dd
KariSims <princesimbaalimasi@esp.sn>
2024-01-07 14:23:43 -0800

    v-0.05

--- Contactels/www/js/index.js
+++ Contactels/www/js/index.js
@@ -1,7 +1,50 @@
 document.addEventListener('deviceready', loadContacts, false);
 
-function createContact(name, phone1,phone2, organization, email){
+function newContact(){
+    const newContact = document.getElementById('registerButton');
+    newContact.onclick = createContact;
+
+    //submit and reset form
+    document.contactForm.submit();
+    document.contactForm.reset();
+}
+
+function createContact(){
     var contact = navigator.contacts.create();
+
+    const firstName = document.getElementsByName('FirstName');
+    const lastName = document.getElementsByName('LastName');
+    const nickName = document.getElementsByName('NickName');
+    const phoneNumbers1 = document.getElementsByName('Number01');
+    const phoneNumbers2 = document.getElementsByName('Number02');
+    const organization = document.getElementsByName('Organization');
+    const date = document.getElementsByName('Date');
+    const email1 = document.getElementsByName('E-mail1');
+    const email2 = document.getElementsByName('E-mail2');
+    const addresse = document.getElementsByName('Addresse');
+
+    var name = new ContactName();
+        name.givenName = firstName;
+        name.familyName = lastName;
+    contact.name = name;
+    contact.nickname = nickName;
+    var phoneNumbers = [];
+        phoneNumbers[0] = new ContactField('mobile', phoneNumbers1, true);
+        phoneNumbers[1] = new ContactField('work', phoneNumbers2, false);
+    contact.phoneNumbers = phoneNumbers;
+    contact.organization = organization;
+    contact.birthday = birthday;
+    var emails = [];
+        emails[0] = new ContactField('e-mail_pro', email1, true);
+        emails[1] = new ContactField('e-mail_other', email2, false);
+    contact.emails = emails;
+    var addresses = [];
+        addresses[0] = new ContactAddresse('', addresse, true);
+    contact.addresses = addresses;
+
+
+    contact.Save(handleContactSuccess, handleContactError);
+    document.contactForm.reset();
 }
 
 function loadContacts(){
@@ -39,6 +82,16 @@ function showContacts(contacts){
     $(contactList).listview('refresh');
 }
 
+function getContact(contactId){
+    let options = new ContactFindOptions();
+    options.filter = contactId;
+    options.hasPhoneNumber = true;
+
+    let fields = ['id'];
+
+    navigator.contacts.find(fields, showContact, handleContactError, options);
+}
+
 function showContact(contacts){
     const contact = contacts[0];
     const contactDetail = document.getElementById('contactDetail');
@@ -71,17 +124,11 @@ function showContact(contacts){
     $(contactDetail).listview('refresh');
 }
 
+function handleContactSuccess(contact){
+    alert("Contact crée avec succès");
+}
+
 function handleContactError(error){
     console.log("Error while getting contacts list");
     console.log(error);
-}
-
-function getContact(contactId){
-    let options = new ContactFindOptions();
-    options.filter = contactId;
-    options.hasPhoneNumber = true;
-
-    let fields = ['id'];
-
-    navigator.contacts.find(fields, showContact, handleContactError, options);
 }
