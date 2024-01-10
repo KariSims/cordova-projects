38415d24a67e6f72bc5448e9297af3fc387a96e4
KariSims <princesimbaalimasi@esp.sn>
2024-01-09 22:30:27 -0800

    v-0.06

--- Contactels/www/js/index.js
+++ Contactels/www/js/index.js
@@ -1,50 +1,87 @@
 document.addEventListener('deviceready', loadContacts, false);
 
 function newContact(){
-    const newContact = document.getElementById('registerButton');
+    let newContact = document.getElementById('register');
     newContact.onclick = createContact;
-
+    
     //submit and reset form
-    document.contactForm.submit();
-    document.contactForm.reset();
+    // document.contactForm.submit();
+    // document.contactForm.reset();
 }
 
+// function createContact(){
+//     //Create
+//     var contact = navigator.contacts.create();
+
+//     //get infos form
+//     const firstName = document.getElementById('FirstName').value;
+//     const lastName = document.getElementById('LastName').value;
+//     const nickName = document.getElementById('NickName').value;
+//     const phoneNumbers1 = document.getElementById('Number01').value;
+//     const phoneNumbers2 = document.getElementById('Number02').value;
+//     const organization = document.getElementById('Organization').value;
+//     const email1 = document.getElementById('E-mail1').value;
+//     const email2 = document.getElementById('E-mail2').value;
+//     const addresse = document.getElementById('Addresse').value;
+    
+//     if (phoneNumbers1){
+//         var names = new ContactName();
+//         names.givenName = firstName;
+//         names.familyName = lastName;
+//         contact.name = names;
+//         contact.nickname = nickName;
+//         var phoneNumbers = [];
+//         phoneNumbers[0] = new ContactField('mobile', phoneNumbers1, true);
+//         phoneNumbers[1] = new ContactField('work', phoneNumbers2, false);
+//         contact.phoneNumbers = phoneNumbers;
+//         contact.organization = organization;
+//         var emails = [];
+//         emails[0] = new ContactField('work', email1, true);
+//         emails[1] = new ContactField('other', email2, false);
+//         contact.emails = emails;
+//         var addresses = [];
+//         addresses[0] = new ContactAddress('', '', addresse, '', '', '', true);
+//         contact.addresses = addresses;
+
+//         contact.save(handleContactSuccess, handleContactError);            
+//     }
+// }
+
 function createContact(){
-    var contact = navigator.contacts.create();
+    //Create
 
-    const firstName = document.getElementsByName('FirstName');
-    const lastName = document.getElementsByName('LastName');
-    const nickName = document.getElementsByName('NickName');
-    const phoneNumbers1 = document.getElementsByName('Number01');
-    const phoneNumbers2 = document.getElementsByName('Number02');
-    const organization = document.getElementsByName('Organization');
-    const date = document.getElementsByName('Date');
-    const email1 = document.getElementsByName('E-mail1');
-    const email2 = document.getElementsByName('E-mail2');
-    const addresse = document.getElementsByName('Addresse');
+    var infosContact ={
+        //get infos form
+        name:{
+            givenName: document.getElementById('FirstName').value,
+            familyName: document.getElementById('LastName').value
+        },
+        // displayName : document.getElementById('FirstName').value + ' ' + document.getElementById('LastName').value,
+        nickName : document.getElementById('NickName').value,
+        phoneNumbers : [{
+            type : 'mobile',
+            value: document.getElementById('Number01').value,
+            type : 'work',
+            value: document.getElementById('Number02').value
+        }],
+        organization : document.getElementById('Organization').value,
+        emails : [{
+            type : 'work',
+            value: document.getElementById('E-mail1').value,
+            type : 'other',
+            value: document.getElementById('E-mail2').value
+        }],
+        addresses :[{
+            type:'work',
+            formatted: document.getElementById('Addresse').value,
+            streetAddress:'',    
+            }] 
+    };
 
-    var name = new ContactName();
-        name.givenName = firstName;
-        name.familyName = lastName;
-    contact.name = name;
-    contact.nickname = nickName;
-    var phoneNumbers = [];
-        phoneNumbers[0] = new ContactField('mobile', phoneNumbers1, true);
-        phoneNumbers[1] = new ContactField('work', phoneNumbers2, false);
-    contact.phoneNumbers = phoneNumbers;
-    contact.organization = organization;
-    contact.birthday = birthday;
-    var emails = [];
-        emails[0] = new ContactField('e-mail_pro', email1, true);
-        emails[1] = new ContactField('e-mail_other', email2, false);
-    contact.emails = emails;
-    var addresses = [];
-        addresses[0] = new ContactAddresse('', addresse, true);
-    contact.addresses = addresses;
-
-
-    contact.Save(handleContactSuccess, handleContactError);
-    document.contactForm.reset();
+    if (infosContact.name && infosContact.phoneNumbers){
+        var contact = navigator.contacts.create(infosContact);
+        contact.save(handleContactSuccess, handleContactError);
+    }
 }
 
 function loadContacts(){
@@ -58,7 +95,7 @@ function loadContacts(){
 }
 
 function showContacts(contacts){
-    let contactHTML = '';
+    // let contactHTML = '';
     let contactItem;
     const contactList = document.getElementById('contactList');
 
@@ -94,6 +131,7 @@ function getContact(contactId){
 
 function showContact(contacts){
     const contact = contacts[0];
+
     const contactDetail = document.getElementById('contactDetail');
     let contactInfo = 
         `
