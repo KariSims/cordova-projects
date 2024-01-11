c658ba5ef57aa1b9340800b8eb882938a8e06942
KariSims <princesimbaalimasi@esp.sn>
2024-01-10 21:58:07 -0800

    v-0.07

--- Contactels/www/js/index.js
+++ Contactels/www/js/index.js
@@ -1,43 +1,50 @@
 document.addEventListener('deviceready', loadContacts, false);
 
+var id_old;
 
-// function createContact(){
-//     //Create
-//     var contact = navigator.contacts.create();
-
-//     //get infos form
-//     const firstName = document.getElementById('FirstName').value;
-//     const lastName = document.getElementById('LastName').value;
-//     const nickName = document.getElementById('NickName').value;
-//     const phoneNumbers1 = document.getElementById('Number01').value;
-//     const phoneNumbers2 = document.getElementById('Number02').value;
-//     const organization = document.getElementById('Organization').value;
-//     const email1 = document.getElementById('E-mail1').value;
-//     const email2 = document.getElementById('E-mail2').value;
-//     const addresse = document.getElementById('Addresse').value;
+function createContact(){
+    const phoneNumbers1 = document.getElementById('Number01').value;
+    const phoneNumbers2 = document.getElementById('Number02').value;
+    const email1 = document.getElementById('E-mail1').value;
+    const email2 = document.getElementById('E-mail2').value;
+    const addresse = document.getElementById('Addresse').value;
     
-//     if (phoneNumbers1){
-//         var names = new ContactName();
-//         names.givenName = firstName;
-//         names.familyName = lastName;
-//         contact.name = names;
-//         contact.nickname = nickName;
-//         var phoneNumbers = [];
-//         phoneNumbers[0] = new ContactField('mobile', phoneNumbers1, true);
-//         phoneNumbers[1] = new ContactField('work', phoneNumbers2, false);
-//         contact.phoneNumbers = phoneNumbers;
-//         contact.organization = organization;
-//         var emails = [];
-//         emails[0] = new ContactField('work', email1, true);
-//         emails[1] = new ContactField('other', email2, false);
-//         contact.emails = emails;
-//         var addresses = [];
-//         addresses[0] = new ContactAddress('', '', addresse, '', '', '', true);
-//         contact.addresses = addresses;
+    var infosContact ={
+        //get infos form
+        name:{
+            givenName: document.getElementById('FirstName').value,
+            familyName: document.getElementById('LastName').value
+        },
+        // displayName : document.getElementById('FirstName').value + ' ' + document.getElementById('LastName').value,
+        nickName : document.getElementById('NickName').value,
+        phoneNumbers : [],
+        emails : [],
+        organization : document.getElementById('Organization').value,
+        addresses : [],
+    };
 
-//         contact.save(handleContactSuccess, handleContactError);            
-//     }
-// }
+    if(phoneNumbers1){
+        infosContact.phoneNumbers.push({type : 'work',value: document.getElementById('Number01').value});
+    }
+    if(phoneNumbers2){
+        infosContact.phoneNumbers.push({type : 'mobile',value: document.getElementById('Number02').value});
+    }
+    if(email1){
+        infosContact.emails.push({type : 'work',value: document.getElementById('E-mail1').value});
+    }
+    if(email2){
+        infosContact.emails.push({type : 'other',value: document.getElementById('E-mail2').value});
+    }
+    if(addresse){
+        infosContact.addresses.push(new ContactAddress('', '', '', '', '', addresse, true));
+    }
+
+    if (infosContact.name && infosContact.phoneNumbers){
+        var contact = navigator.contacts.create(infosContact);
+        contact.save(registerContactSuccess("crée", "page-contact-list"), handleContactError);
+        document.contactForm.reset();
+    }
+}
 
 function loadContacts(){
     let options = new ContactFindOptions();
@@ -48,7 +55,7 @@ function loadContacts(){
 
     navigator.contacts.find(fields, showContacts, handleContactError, options);
     
-    // Ajoutez un gestionnaire d'événement au bouton ou à l'endroit où vous souhaitez déclencher le retour à l'accueil
+    // Ajoutez un événement au bouton pour retourner à l'accueil
     let boutonAccueil = document.getElementById('home');
     boutonAccueil.addEventListener('click', function(){
         urlAccueil = 'http://example.com/accueil-android';  
@@ -96,12 +103,13 @@ function showContact(contacts){
 
     const contactDetail = document.getElementById('contactDetail');
     const deleteContact = document.getElementById('deleteContact');
+    const editContact = document.getElementById('iconEditContact');
 
     let contactInfo = 
         `
         <li>
             <img src="assets/img/contact.png" alt="contact">
-            <h1>Nom du Contact</h1>
+            <h1>Nom du Contact </h1>
             <p>${contact.name.formatted}</p>
         </li>
         <li>
@@ -128,49 +136,77 @@ function showContact(contacts){
     deleteContact.onclick = function(){
         getContactDelete(contact.id);
     };
+
+    editContact.onclick = function(){
+        id_old = contact.id;
+        alert("id : " + id_old);
+    };
 }
 
-function createContact(){
-    const phoneNumbers1 = document.getElementById('Number01').value;
-    const phoneNumbers2 = document.getElementById('Number02').value;
-    const email1 = document.getElementById('E-mail1').value;
-    const email2 = document.getElementById('E-mail2').value;
-    const addresse = document.getElementById('Addresse').value;
-    
-    var infosContact ={
-        //get infos form
-        name:{
-            givenName: document.getElementById('FirstName').value,
-            familyName: document.getElementById('LastName').value
-        },
-        // displayName : document.getElementById('FirstName').value + ' ' + document.getElementById('LastName').value,
-        nickName : document.getElementById('NickName').value,
-        phoneNumbers : [],
-        emails : [],
-        organization : document.getElementById('Organization').value,
-        addresses : [],
-    };
+function toEditContact(){
+    let options = new ContactFindOptions();
+    options.filter = id_old;
+    options.multiple = false;
+    options.hasPhoneNumber = true;
 
-    if(phoneNumbers1){
-        infosContact.phoneNumbers.push({type : 'work',value: document.getElementById('Number01').value});
-    }
-    if(phoneNumbers2){
-        infosContact.phoneNumbers.push({type : 'mobile',value: document.getElementById('Number01').value});
-    }
-    if(email1){
-        infosContact.emails.push({type : 'work',value: document.getElementById('E-mail1').value});
-    }
-    if(email2){
-        infosContact.emails.push({type : 'other',value: document.getElementById('E-mail2').value});
-    }
-    if(addresse){
-        infosContact.addresses.push(new ContactAddress('', '', '', '', '', addresse, true));
-    }
+    let fields = ['id'];
 
-    if (infosContact.name && infosContact.phoneNumbers){
-        var contact = navigator.contacts.create(infosContact);
-        contact.save(registerContactSuccess, handleContactError);
-        document.contactForm.reset();
+    navigator.contacts.find(fields, updateContact, handleContactError, options);
+
+    function updateContact(contacts){
+        if (contacts.length > 0) {
+            var contact = contacts[0];
+
+            const editPhoneNumbers1 = document.getElementById('editNumber01').value;
+            const editPhoneNumbers2 = document.getElementById('editNumber02').value;
+            const editEmail1 = document.getElementById('editE-mail1').value;
+            const editEmail2 = document.getElementById('editE-mail2').value;
+            const editAddresse = document.getElementById('editAddresse').value;
+            
+            var infosEditContact ={
+                name:{
+                    givenName: document.getElementById('editFirstName').value,
+                    familyName: document.getElementById('editLastName').value
+                },
+                nickName : document.getElementById('editNickName').value,
+                phoneNumbers : [],
+                emails : [],
+                organization : document.getElementById('editOrganization').value,
+                addresses : [],
+            };
+
+            if(editPhoneNumbers1){
+                infosEditContact.phoneNumbers.push({type : 'work',value: editPhoneNumbers1});
+            }
+            if(editPhoneNumbers2){
+                infosEditContact.phoneNumbers.push({type : 'mobile',value: editPhoneNumbers2});
+            }
+            if(editEmail1){
+                infosEditContact.emails.push({type : 'work',value: editEmail1});
+            }
+            if(editEmail2){
+                infosEditContact.emails.push({type : 'other',value: editEmail2});
+            }
+            if(editAddresse){
+                infosEditContact.addresses.push(new ContactAddress('', '', '', '', '', editAddresse, true));
+            }
+            
+            if (infosEditContact.phoneNumbers){
+                infosEditContact.id = contact.id;
+                contact.name = infosEditContact.name;
+                contact.nickName = infosEditContact.nickName;
+                contact.organization = infosEditContact.organization;
+                contact.phoneNumbers = infosEditContact.phoneNumbers;
+                contact.emails = infosEditContact.emails;
+                contact.addresses = infosEditContact.addresses;
+                
+                contact.save(registerContactSuccess("modifié", "page-contact-list"), handleContactError);
+                document.contactFormEdit.reset();
+                location.reload();
+            }
+        }else{
+            alert("Contact non trouvé");
+        }
     }
 }
 
@@ -186,15 +222,18 @@ function getContactDelete(contactId){
     function contactfindSuccess(contacts) {
         if (contacts.length > 0) {
             var contact = contacts[0];
-            contact.remove(contactRemoveSuccess, contactRemoveError);
+            let answer = confirm('Are you sure you want to delete this?');
+            if (answer){
+                contact.remove(contactRemoveSuccess, contactRemoveError);
+            }
         } else {
             alert("Contact non trouvé");
         }
         
        function contactRemoveSuccess() {
-          alert("Contact supprimé");
-          window.location.href = '#page-contact-list';
-          location.reload();
+            alert("Contact supprimé avec succès");
+            window.location.href = '#page-contact-list';
+            location.reload();
        }
  
        function contactRemoveError(message) {
@@ -203,9 +242,9 @@ function getContactDelete(contactId){
     }     
 }
 
-function registerContactSuccess(contact){
-    alert("Contact crée avec succès");
-    window.location.href="#page-contact-list";
+function registerContactSuccess(msg, redirectPage){
+    alert("Contact " + msg + " avec succès");
+    window.location.href="#" + redirectPage;
     location.reload();
 }
 
@@ -213,3 +252,41 @@ function handleContactError(error){
     console.log("Error while getting contacts list");
     console.log(error);
 }
+
+// function createContact(){
+//     //Create
+//     var contact = navigator.contacts.create();
+
+//     //get infos form
+//     const firstName = document.getElementById('FirstName').value;
+//     const lastName = document.getElementById('LastName').value;
+//     const nickName = document.getElementById('NickName').value;
+//     const phoneNumbers1 = document.getElementById('Number01').value;
+//     const phoneNumbers2 = document.getElementById('Number02').value;
+//     const organization = document.getElementById('Organization').value;
+//     const email1 = document.getElementById('E-mail1').value;
+//     const email2 = document.getElementById('E-mail2').value;
+//     const addresse = document.getElementById('Addresse').value;
+    
+//     if (phoneNumbers1){
+//         var names = new ContactName();
+//         names.givenName = firstName;
+//         names.familyName = lastName;
+//         contact.name = names;
+//         contact.nickname = nickName;
+//         var phoneNumbers = [];
+//         phoneNumbers[0] = new ContactField('mobile', phoneNumbers1, true);
+//         phoneNumbers[1] = new ContactField('work', phoneNumbers2, false);
+//         contact.phoneNumbers = phoneNumbers;
+//         contact.organization = organization;
+//         var emails = [];
+//         emails[0] = new ContactField('work', email1, true);
+//         emails[1] = new ContactField('other', email2, false);
+//         contact.emails = emails;
+//         var addresses = [];
+//         addresses[0] = new ContactAddress('', '', addresse, '', '', '', true);
+//         contact.addresses = addresses;
+
+//         contact.save(handleContactSuccess, handleContactError);            
+//     }
+// }
